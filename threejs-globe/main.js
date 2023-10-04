b0fd4ab57587b3552a7cc4058c08287fc5dbd188
Alsaaram <131556435+Alsaaram@users.noreply.github.com>
2023-10-04 16:25:44 -0400

    2D globe with texture, shader and vite config additions

--- threejs-globe/main.js
+++ threejs-globe/main.js
@@ -1,5 +1,46 @@
-import './style.css'
 import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 
-const scene = new THREE.Scene();
-console.log(scene);
+const scene = new THREE.Scene()
+const camera = new THREE.
+  PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    0.1,
+    1000
+  );
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+  renderer.setSize(innerWidth, innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  document.body.appendChild(renderer.
+    domElement)
+
+//sphere
+const sphere = new THREE.Mesh(new THREE.
+  SphereGeometry(5, 50, 50), new THREE.
+  ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load('./img/Earth-plantlife.png') //world map, change here
+      }
+    }
+  })
+  
+)
+
+scene.add(sphere);
+
+camera.position.z = 15;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate()
